dd6600ce3a94df7b464e2669db3c8d95210078d9
Sergio Jiménez Herena <sergio.jimenez.herena@gmail.com>
2017-07-24 15:27:00 +0200

    Improve build.py
    · Formated code to adhere to PEP8 style guide
    
    · 'input', 'vars', and 'type' variables/arguments renamed, since these
    are python built-in function names and should't be used for other
    purposes
    
    · Fix bug in main(): If 'dist' dir doesn't exist create it before trying
    to write files to it

--- build.py
+++ build.py
@@ -2,6 +2,7 @@
 
 import sys
 import re
+from os import path, makedirs
 
 files = [
     "acsversion.acs",
@@ -25,65 +26,79 @@ files = [
 ]
 
 TYPE_ALIASES_ACC = {
-    #'int': 'int',
+    # 'int': 'int',
     'num': 'int',
     'fixed': 'int',
     'ang': 'int',
     'raw': 'int',
     
-    #'bool': 'bool',
+    # 'bool': 'bool',
     
-    #'str': 'str',
+    # 'str': 'str',
 }
 
 TYPE_ALIASES_BCC = {
-    #'int': 'int',
-    #'fixed': 'fixed',
-    #'bool': 'bool',
+    # 'int': 'int',
+    # 'fixed': 'fixed',
+    # 'bool': 'bool',
     'ang': 'fixed',
     'num': 'raw',
-    #'raw': 'raw',
+    # 'raw': 'raw',
     'str': 'str',
 }
+
 
 def readfile(*args, **kwargs):
     with open(*args, **kwargs) as file:
         return file.read()
+
 
 def error(string):
     print(string, file=sys.stderr)
     sys.exit(1)
 
 # Global variable stuff
-PRIVCONST_DECLARATION = re.compile(r'@privconst\s+(\w+)\s+(\w+)(\s*=\s*([^;]+))?\s*;', flags=re.IGNORECASE) 
-CONST_DECLARATION = re.compile(r'@const\s+(\w+)\s+(\w+)(\s*=\s*([^;]+))?\s*;', flags=re.IGNORECASE) 
-VAR_DECLARATION = re.compile(r'@(\w+)\s+(\w+)(\s*=\s*([^;]+))?\s*;', flags=re.IGNORECASE) 
+PRIVCONST_DECLARATION = \
+    re.compile(r'@privconst\s+(\w+)\s+(\w+)(\s*=\s*([^;]+))?\s*;',
+               flags=re.IGNORECASE)
+
+CONST_DECLARATION = \
+    re.compile(r'@const\s+(\w+)\s+(\w+)(\s*=\s*([^;]+))?\s*;',
+               flags=re.IGNORECASE)
+
+VAR_DECLARATION = \
+    re.compile(r'@(\w+)\s+(\w+)(\s*=\s*([^;]+))?\s*;',
+               flags=re.IGNORECASE)
+
 VAR_USE = re.compile(r'\$(\w+)', flags=re.IGNORECASE)
+
 
 class Var:
     pass
-    
+
+
 def find_global_vars():
-    vars = {}
+    global_variables = {}
     
     for filename in files:
-        input = readfile('src/' + filename, 'r', encoding='utf-8')
-        for match in VAR_DECLARATION.finditer(input):
+        file_content = readfile('src/' + filename, 'r', encoding='utf-8')
+        for match in VAR_DECLARATION.finditer(file_content):
             var = Var()
             var.type = match.group(1).lower()
             var.name = match.group(2)
             var.value = match.group(4).strip()
             
-            if var.name.lower() in vars:
+            if var.name.lower() in global_variables:
                 error('%s already declared' % var.name)    
-            vars[var.name.lower()] = var
+            global_variables[var.name.lower()] = var
             
-    return vars
-    
-def allocate_global_vars(vars):
+    return global_variables
+
+
+def allocate_global_vars(global_variables):
     ints = []
     strs = []
-    for var in vars.values():
+    for var in global_variables.values():
         if var.type == 'str':
             var.index = len(strs)
             strs.append(var)
@@ -92,9 +107,10 @@ def allocate_global_vars(vars):
             ints.append(var)
             
     return ints, strs
-    
-def write_global_vars(out, ints, strs, type):
-    out.write('%s ACSUtils_Ints[%d] = {\n' % (type, len(ints)))
+
+
+def write_global_vars(out, ints, strs, var_type):
+    out.write('%s ACSUtils_Ints[%d] = {\n' % (var_type, len(ints)))
     for var in ints:
         if var.type != 'str':
             out.write('\t%s, // %s\n' % (var.value, var.name))
@@ -105,58 +121,71 @@ def write_global_vars(out, ints, strs, type):
         if var.type == 'str':
             out.write('\t%s, // %s\n' % (var.value, var.name))
     out.write('};\n\n')
-    
-def replace_var_uses(input, vars):                
+
+
+def replace_var_uses(file_content, global_variables):
     def repl(match):
         name = match.group(1)
-        var = vars.get(name.lower());
+        var = global_variables.get(name.lower())
         if not var:
-            error('$%s not found' % name);
+            error('$%s not found' % name)
         
         if var.type == 'str':
             return 'ACSUtils_Strings[%d]' % var.index
         else:
             return 'ACSUtils_Ints[%d]' % var.index
 
-    return VAR_USE.sub(repl, input)
+    return VAR_USE.sub(repl, file_content)
    
    
-def remove_variable_declarations(input):
-    input = VAR_DECLARATION.sub('', input)
-    return input
-    
-def replace_constants_acc(input):
-    input = CONST_DECLARATION.sub(r'#libdefine \2 \4', input)
-    input = PRIVCONST_DECLARATION.sub(r'#define \2 \4', input)
-    return input
-    
-def replace_constants_bcc(input):
-    input = CONST_DECLARATION.sub(r'enum : \1 { \2 = \4 };', input)
-    input = PRIVCONST_DECLARATION.sub(r'private enum : \1 { \2 = \4 };', input)
-    return input
-    
-# Type replacement
-def remove_casts(input):
-    return re.sub(r'\b(int|fixed|str|bool|ang)\(', '(', input)
+def remove_variable_declarations(file_content):
+    file_content = VAR_DECLARATION.sub('', file_content)
+    return file_content
 
-def replace_casts(input):
-    return re.sub(r'\b(int|fixed|str|bool|ang)\(', r'(\1)(', input)
-   
-def replace_types_acc(input):
+
+def replace_constants_acc(file_content):
+    file_content = CONST_DECLARATION.sub(r'#libdefine \2 \4', file_content)
+    file_content = PRIVCONST_DECLARATION.sub(r'#define \2 \4', file_content)
+    return file_content
+
+
+def replace_constants_bcc(file_content):
+    file_content = CONST_DECLARATION.sub(r'enum : \1 { \2 = \4 };',
+                                         file_content)
+    file_content = PRIVCONST_DECLARATION.sub(r'private enum : \1 { \2 = \4 };',
+                                             file_content)
+    return file_content
+
+
+# Type replacement
+def remove_casts(file_content):
+    return re.sub(r'\b(int|fixed|str|bool|ang)\(', '(', file_content)
+
+
+def replace_casts(file_content):
+    return re.sub(r'\b(int|fixed|str|bool|ang)\(', r'(\1)(', file_content)
+
+
+def replace_types_acc(file_content):
     for a, b in TYPE_ALIASES_ACC.items():
-        input = re.sub(r'\b' + a + r'\b', b, input)
-    return input
-   
-def replace_types_bcc(input):
+        file_content = re.sub(r'\b' + a + r'\b', b, file_content)
+    return file_content
+
+
+def replace_types_bcc(file_content):
     for a, b in TYPE_ALIASES_BCC.items():
-        input = re.sub(r'\b' + a + r'\b', b, input)
-    return input
-   
+        file_content = re.sub(r'\b' + a + r'\b', b, file_content)
+    return file_content
+
+
 def main():
-    vars = find_global_vars()
-    ints, strs = allocate_global_vars(vars)
-    
+    global_vars = find_global_vars()
+    ints, strs = allocate_global_vars(global_vars)
+
     # generate output file
+    if not path.isdir('dist'):
+        makedirs('dist')
+
     acsutils = open('dist/acsutils.acs', 'w', encoding='utf-8')
     bcsutils = open('dist/bcsutils.bcs', 'w', encoding='utf-8')
     
@@ -168,10 +197,10 @@ def main():
     
     # write ACSUtils code
     for filename in files:
-        input = readfile('src/' + filename, 'r', encoding='utf-8')
-        input = replace_var_uses(input, vars)
+        file_content = readfile('src/' + filename, 'r', encoding='utf-8')
+        file_content = replace_var_uses(file_content, global_vars)
         
-        acc_input = input
+        acc_input = file_content
         acc_input = remove_casts(acc_input)
         acc_input = replace_constants_acc(acc_input)
         acc_input = remove_variable_declarations(acc_input)
@@ -179,7 +208,7 @@ def main():
         acsutils.write(acc_input)
         acsutils.write('\n')
         
-        bcc_input = input
+        bcc_input = file_content
         bcc_input = replace_casts(bcc_input)
         bcc_input = replace_constants_bcc(bcc_input)
         bcc_input = remove_variable_declarations(bcc_input)
